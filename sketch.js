36b70f013fe3a48700aa1ba5ac513a561bf31f0c
Anish Sundaram <anishs2410@gmail.com>
2020-12-18 19:42:56 +0530

    Bird changed

--- sketch.js
+++ sketch.js
@@ -35,7 +35,7 @@ platform = new Ground(150,305,300,170)
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(100,150);
 
 }
 
